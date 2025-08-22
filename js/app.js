ff3c1d83f2cf66b7e1516dec043081d5fef224db
KTP-Abenteuerland <nikladyk123@gmail.com>
2025-08-22 18:07:13 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -17,6 +17,33 @@ window.addEventListener("resize", function () {
   }
 });
 
+function openSpr() {
+  var test = window.innerWidth*0.6;
+  document.getElementById("sprache").style.width = test.toString()+"px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeSpr() {
+  document.getElementById("sprache").style.width = "0";
+}
+
+window.addEventListener("resize", function () {
+  const nav = document.getElementById("top");
+
+  if (window.innerWidth >= 961) {
+    // Inline-Style entfernen, damit Media Query wieder greift
+    nav.style.display = "";
+  }
+});
+
+  function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 
 
